4ac7d34508e17716fbc92a6f03e3751f064b1c4a
VicAbreuPro <victor.abreu09.pro@gmail.com>
2022-03-17 09:55:45 +0000

    up final

--- Data/Conn.cs
+++ Data/Conn.cs
@@ -4,11 +4,11 @@ namespace API_ES.Data
     public class Conn
     {
         // Variáveis Auxiliares para utilização dos dados de acesso ao servidor
-        private const string servidor = "myvnadb.mysql.database.azure.com";
-        private const string port = "3306";
-        private const string schemaAuth = "auth";
-        private const string usuario = "jynx";
-        private const string senha = "Hummerh14x4rally";
+        private const string servidor = "";
+        private const string port = "";
+        private const string schemaAuth = "";
+        private const string usuario = "";
+        private const string senha = "";
 
         static public string strConn = $"server={servidor};port={port};User Id={usuario};database={schemaAuth};password={senha}";
     }
